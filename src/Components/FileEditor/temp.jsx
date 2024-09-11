43af1fd4c1862662f5da3cb5ea2676d6160fd16e
Mursalin <mursalin2311@gmail.com>
2024-09-12 04:52:20 +0600

    Alhamdulillah all good

--- src/Components/FileEditor/temp.jsx
+++ src/Components/FileEditor/temp.jsx
@@ -1,16 +1,14 @@
 "use client";
 import { useState, useEffect } from "react";
 import useSWR, { mutate } from "swr";
-import dynamic from "next/dynamic"; // dynamic for client-side only components
+import dynamic from "next/dynamic"; //  dynamic for client-side only components
 import { fetcher } from "@/lib/fetcher";
 import "./FileEditor.css";
-import { useRouter } from "next/navigation";
 
 // Dynamically import ReactQuill to disable SSR
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-import "react-quill/dist/quill.snow.css"; // Quill styles
+import "react-quill/dist/quill.snow.css"; // t Quill styles
 import { toast } from "react-toastify";
-import Image from "next/image";
 
 const FileEditor = ({ fileId }) => {
   useEffect(() => {
@@ -21,7 +19,6 @@ const FileEditor = ({ fileId }) => {
       Quill.default.register(SizeStyle, true);
     });
   }, []);
-  const router = useRouter();
 
   const { data, error, isLoading } = useSWR(
     `http://localhost:5000/api/v1/files/single/${fileId}`,
@@ -29,16 +26,12 @@ const FileEditor = ({ fileId }) => {
   );
 
   const [editorContent, setEditorContent] = useState("");
-  const [imageOne, setImageOne] = useState("");
-  const [imageTwo, setImageTwo] = useState("");
   const [loading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false);
 
   useEffect(() => {
-    if (data?.data) {
-      setEditorContent(data?.data?.htmlContent || "");
-      setImageOne(data?.data?.imageOne || "");
-      setImageTwo(data?.data?.imageTwo || "");
+    if (data?.data?.htmlContent) {
+      setEditorContent(data?.data?.htmlContent);
     }
   }, [data]);
 
@@ -51,11 +44,7 @@ const FileEditor = ({ fileId }) => {
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              htmlContent: editorContent,
-              imageOne,
-              imageTwo,
-            }),
+            body: JSON.stringify({ htmlContent: editorContent }),
           }
         );
 
@@ -92,16 +81,14 @@ const FileEditor = ({ fileId }) => {
         );
 
         if (response.ok) {
-          toast.success("File deleted successfully!");
+          alert("File deleted successfully!");
+          // You can redirect the user or trigger a re-render here
           mutate(`http://localhost:5000/api/v1/files/single/${fileId}`);
-          router.push("/edit");
         } else {
           console.error("Failed to delete file:", await response.text());
-          toast.error("Failed to delete file:");
         }
       } catch (error) {
         console.error("Error deleting file:", error);
-        toast.error("Error deleting file:", error);
       } finally {
         setDeleting(false);
       }
@@ -117,62 +104,15 @@ const FileEditor = ({ fileId }) => {
   if (error)
     return (
       <div className="text-center h-screen text-red-600 flex justify-center items-center font-semibold">
-        Error! Failed to load file 😢
+        Error ! Failed to load file 😢
       </div>
     );
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 p-6">
-      <div className="p-8 rounded-lg shadow-lg w-full md:w-[60%] bg-white text-gray-700">
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 p-6 ">
+      <div className="p-8 rounded-lg shadow-lg w-full md:w-[60%]  bg-white text-gray-700 ">
         <div>
           <h2 className="text-xl mb-4 font-semibold">Edit Content</h2>
-          {/* Image One Input and Preview */}
-          <label className="block mb-2 font-medium text-gray-700">
-            Image One
-          </label>
-          {imageOne && (
-            <div className="mb-4">
-              <Image
-                src={imageOne}
-                alt="Image One"
-                width={720}
-                height={1080}
-                className="max-w-[400px] max-h-[400px] object-contain rounded mb-2 mx-auto"
-              />
-            </div>
-          )}
-          <input
-            type="text"
-            placeholder="Enter Image One URL"
-            value={imageOne}
-            onChange={(e) => setImageOne(e.target.value)}
-            className="w-full p-2 mb-4 border rounded-md border-gray-300"
-          />
-
-          {/* Image Two Input and Preview */}
-          <label className="block mb-2 font-medium text-gray-700">
-            Image Two
-          </label>
-          {imageTwo && (
-            <div className="mb-4">
-              <Image
-                src={imageTwo}
-                alt="Image Two"
-                width={720}
-                height={1080}
-                className="max-w-[400px] max-h-[400px] object-contain rounded mb-2 mx-auto"
-              />
-            </div>
-          )}
-          <input
-            type="text"
-            placeholder="Enter Image Two URL"
-            value={imageTwo}
-            onChange={(e) => setImageTwo(e.target.value)}
-            className="w-full p-2 mb-4 border rounded-md border-gray-300"
-          />
-
-          {/* Text Editor */}
           <ReactQuill
             value={editorContent}
             onChange={setEditorContent}
@@ -219,7 +159,7 @@ const FileEditor = ({ fileId }) => {
               {deleting ? "Deleting..." : "Delete File"}
             </button>
             <button
-              className="mt-4 px-4 py-2 border border-green-600 text-green-600 hover:text-white font-semibold rounded-md hover:bg-green-700 transition-colors duration-500"
+              className="mt-4 px-4 py-2 border border-green-600 text-green-600 hover:text-white font-semibold  rounded-md hover:bg-green-700 transition-colors duration-500"
               onClick={handleSave}
               disabled={loading}
             >
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,9 @@
 import getAllBlogs from "@/lib/getAllBlogs";
+import Image from "next/image";
 
 const BlogMainPage = async () => {
   const blogs = await getAllBlogs();
 
- 
   if (blogs?.data?.length === 0) {
     return (
       <div className="text-gray-100 h-screen flex justify-center items-center">
@@ -13,13 +13,39 @@ const BlogMainPage = async () => {
   }
 
   return (
-    <div className="p-4 space-y-4 bg-white text-gray-900 h-max ">
+    <div className="p-4 space-y-4 bg-white text-gray-900 overflow-auto">
       {blogs?.data?.map((blog) => (
         <div
           key={blog._id}
-          className=" border-gray-200 pb-4 w-[70%] mx-auto mb-10"
+          className="border-gray-200 pb-4 w-[50%] mx-auto mb-10"
         >
-          <div dangerouslySetInnerHTML={{ __html: blog.htmlContent }} />
+          <div>
+            <Image
+              src={blog?.imageOne}
+              width={600}
+              height={600}
+              alt="Image one"
+              className="mx-auto mb-10 w-full h-auto"
+            />
+          </div>
+          
+          <div className="relative">
+            {/* Float imageTwo to the left */}
+            <div className="float-left mr-4 mb-2">
+              <Image
+                src={blog?.imageTwo}
+                width={300}  // Adjust width as necessary
+                height={300}
+                alt="Image two"
+                className="block p-3"
+              />
+            </div>
+            {/* Blog content will wrap around the image */}
+            <div dangerouslySetInnerHTML={{ __html: blog.htmlContent }} />
+          </div>
+
+          {/* Clear the float to avoid overlapping */}
+          <div className="clear-both"></div>
         </div>
       ))}
     </div>
