606cb5b6be4b88a47d76e416c7030094bb9311a3
Mursalin <mursalin2311@gmail.com>
2024-09-14 22:50:52 +0600

    added toggle cool button but need to modify some pages

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaHome, FaUpload, FaEdit, FaBars, FaTimes } from "react-icons/fa";
-import ThemeToggleButton from "@/Components/Theme/Theme";
+import ButtonToggleLightAndDark from "@/Components/Ui/Buttons/ButtonToggleLightAndDark";
 
 const navItems = [
   { name: "Home", href: "/", icon: <FaHome size={20} /> },
@@ -24,44 +24,50 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gray-800 text-white shadow-md">
-      <div className="container  flex justify-start items-center p-4 gap-12">
-        {/* Logo Section */}
-        <div className="text-3xl font-bold">
-          <Link href="/" className="text-primary">
-            SheetStream
-          </Link>
-        </div>
-
-        {/* Mobile Menu Toggle */}
-        <button
-          className="text-2xl lg:hidden"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </button>
-
-        {/* Navigation Items for Desktop */}
-        <div className="hidden lg:flex gap-8">
-          {navItems.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className={`flex items-center space-x-2 text-sm ${
-                isActive(item.href) ? "text-primaryLight" : "text-gray-400 hover:text-primaryDark"
-              }`}
-            >
-              {item.icon}
-              <span>{item.name}</span>
+      <div className="flex items-center justify-between ms-5 me-5">
+        <div className="container  flex justify-start items-center p-4 gap-12">
+          {/* Logo Section */}
+          <div className="text-3xl font-bold">
+            <Link href="/" className="text-primary">
+              SheetStream
             </Link>
-          ))}
+          </div>
+
+          {/* Mobile Menu Toggle */}
+          <button
+            className="text-2xl lg:hidden"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+
+          {/* Navigation Items for Desktop */}
+          <div className="hidden lg:flex gap-8">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className={`flex items-center space-x-2 text-sm ${
+                  isActive(item.href)
+                    ? "text-primaryLight"
+                    : "text-gray-400 hover:text-primaryDark"
+                }`}
+              >
+                {item.icon}
+                <span>{item.name}</span>
+              </Link>
+            ))}
+          </div>
         </div>
-        <ThemeToggleButton/>
+        <ButtonToggleLightAndDark />
       </div>
 
       {/* Mobile Menu */}
       <div
         className={`lg:hidden transition-all duration-300 ease-in-out transform origin-top ${
-          menuOpen ? "max-h-[400px] opacity-100 scale-y-100" : "max-h-0 opacity-0 scale-y-0"
+          menuOpen
+            ? "max-h-[400px] opacity-100 scale-y-100"
+            : "max-h-0 opacity-0 scale-y-0"
         } overflow-hidden`}
       >
         <div className="flex flex-col space-y-4 bg-gray-800 p-4">
