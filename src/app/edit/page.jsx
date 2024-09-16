106f661696bca75abbad1a24a613566276948c3e
Mursalin <mursalin2311@gmail.com>
2024-09-17 02:11:23 +0600

    made edit page table and more responsive

--- src/app/edit/page.jsx
+++ src/app/edit/page.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { AiOutlineEdit } from "react-icons/ai";
 import getAllBlogs from "@/lib/getAllBlogs";
+import { format } from "date-fns";
 
 const EditFileMainPage = async () => {
   const blogs = await getAllBlogs();
@@ -13,34 +14,58 @@ const EditFileMainPage = async () => {
     );
   }
 
-  //console.log(blogs);
-
   return (
     <div className="p-6 space-y-6 h-screen flex flex-col items-center">
-      <h1 className="text-white text-2xl font-bold mb-8">All Files</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogs?.data?.map((blog) => (
-          <div
-            key={blog?._id}
-            className="flex flex-col items-center justify-between bg-gray-800 text-white shadow-md hover:shadow-xl hover:bg-gray-700 transition-all duration-300 rounded-lg p-6 w-full max-w-xs"
-          >
-            <div className="w-full mb-2">
-              <div
-                className={`w-[30%]  h-3 rounded-full  mx-auto  ${
-                  blog?.isOnline ? "bg-green-600" : "bg-red-700"
-                }`}
-              ></div>
-            </div>
-            <p className="truncate w-full text-lg font-medium mb-4">
-              {blog?.fileName}
-            </p>
-            <Link href={`/edit/${blog?._id}`} className="w-full">
-              <button className="w-full text-sm font-semibold text-blue-500 flex items-center justify-center gap-2 bg-gray-900 border border-blue-600 py-2 rounded-lg hover:bg-blue-600 hover:text-white transition-all duration-300">
-                <AiOutlineEdit className="text-lg" /> Edit File
-              </button>
-            </Link>
-          </div>
-        ))}
+      <h1 className="text-2xl font-bold mb-8  dark:text-white">All Files</h1>
+      <div className="w-full max-w-6xl">
+        <table className="w-full table-auto border-collapse border border-gray-200 dark:border-gray-700 cursor-pointer">
+          <thead>
+            <tr className="bg-primary text-white text-sm">
+              <th className="border border-gray-300 dark:border-gray-700 py-2 px-4 text-left">
+                File Name
+              </th>
+              <th className="border border-gray-300 dark:border-gray-700 py-2 px-4 text-center hidden lg:block">
+                Date
+              </th>
+              <th className="border border-gray-300 dark:border-gray-700 py-2 px-4 text-left">
+                Status
+              </th>
+              <th className="border border-gray-300 dark:border-gray-700 py-2 px-4 text-left">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs?.data?.map((blog) => (
+              <tr
+                key={blog?._id}
+                className="hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200"
+              >
+                <td className="border border-gray-300 dark:border-gray-700 py-2 px-4 font-semibold">
+                  {blog?.fileName}
+                </td>
+                <td className="border border-gray-300 dark:border-gray-700 py-2 px-4 font-semibold text-center hidden lg:block">
+                  {format(new Date(blog.uploadDate), "d MMMM yyyy 'at' h:mm a")}
+                </td>
+                <td className="border border-gray-300 dark:border-gray-700 py-2 px-4">
+                  <span
+                    className={`inline-block w-3 h-3 rounded-full mr-2 ${
+                      blog?.isOnline ? "bg-green-600" : "bg-red-600"
+                    }`}
+                  ></span>
+                  {blog?.isOnline ? "Online" : "Offline"}
+                </td>
+                <td className="border border-red-600 dark:border-gray-700 py-2 px-4 flex justify-center">
+                  <Link href={`/edit/${blog?._id}`}>
+                    <button className="text-sm font-semibold text-blue-500 dark:text-blue-400 flex items-center gap-2 bg-gray-200 dark:bg-gray-900 border border-blue-600 py-1 px-3 rounded hover:bg-blue-600 hover:text-white transition-all duration-300">
+                      <AiOutlineEdit className="text-lg" /> Edit
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
