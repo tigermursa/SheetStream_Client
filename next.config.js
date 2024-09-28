3534cb27087d7b63410fdd07623cb501ee39ccb3
Mursalin <mursalin2311@gmail.com>
2024-09-28 21:20:48 +0600

    so the token is geeting really massy so have to fix it

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
     ],
+    domains: ["res.cloudinary.com"],
   },
 };
 
