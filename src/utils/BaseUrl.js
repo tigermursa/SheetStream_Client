1e4a1cd6388eeda9d9a54f55f9e674d4e8bcfa8c
Mursalin <mursalin2311@gmail.com>
2024-11-29 11:28:15 +0600

    ok but need data

--- src/utils/BaseUrl.js
+++ src/utils/BaseUrl.js
@@ -1,8 +1,5 @@
- const BASE_URL = "https://sheetstreamserver-production.up.railway.app";
- export default BASE_URL;
+//  const BASE_URL = "https://sheetstreamserver-production.up.railway.app";
+//  export default BASE_URL;
 
-
-// const BASE_URL = "http://localhost:5000";
-// export default BASE_URL;
-
-
+const BASE_URL = "http://localhost:5000";
+export default BASE_URL;
